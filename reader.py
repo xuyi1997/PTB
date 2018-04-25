760b2e170aa5150a1b0c7ce8f4614156ae2f199f
xuyi1997 <38726507+xuyi1997@users.noreply.github.com>
2018-04-25 15:46:59 +0000

    Update reader.py

--- reader.py
+++ reader.py
@@ -66,9 +66,9 @@ def ptb_raw_data(data_path=None):
     where each of the data objects can be passed to PTBIterator.
   """
 
-  train_path = os.path.join(data_path, "ptb.train.txt")
-  valid_path = os.path.join(data_path, "ptb.valid.txt")
-  test_path = os.path.join(data_path, "ptb.test.txt")
+  train_path = os.path.join( "ptb.train.txt")
+  valid_path = os.path.join( "ptb.valid.txt")
+  test_path = os.path.join( "ptb.test.txt")
 
   word_to_id = _build_vocab(train_path)
   train_data = _file_to_word_ids(train_path, word_to_id)
